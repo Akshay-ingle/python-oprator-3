fde4b40ebb5cb7df511124773f5832a6d6e1fc8b
Akshay-ingle <akshy.nilesh.ingle@gmail.com>
2024-11-15 19:20:48 +0530

    oprator 3

---
+++ identityoprator.py
@@ -0,0 +1,24 @@
+a=2
+
+if (type(a) is int):
+    print('true')
+
+else:
+    print('false')
+
+b=9.10
+
+if (type(b) is float):
+    print('true')
+
+else:
+    print('false')
+
+c=67
+d=67
+
+if c is d:
+    print('true')
+
+else:
+    print('false')
